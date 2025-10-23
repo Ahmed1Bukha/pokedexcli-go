a3fb6bbd2d8b05986c03f0e432f9b9ff610fc0bd
AhmedBukha <56551464+Ahmed1Bukha@users.noreply.github.com>
2025-10-24 01:50:45 +0300

    Refactor code

--- main.go
+++ main.go
@@ -7,17 +7,48 @@ import (
 	"strings"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+
 func main(){
+	cliCommands:= getCommands()
 	scanner := bufio.NewScanner(os.Stdin)
 	for {
 		fmt.Print("Pokedex > ")
 		scanner.Scan()
-		word:= scanner.Text()
-		cleanedInput:= cleanInput(word)
-		fmt.Println("Your command was: "+cleanedInput[0])
+		words:= scanner.Text()
+		if len(words) == 0 {
+			continue
+		}
+		cleanedInput:= cleanInput(words)
+		value,ok := cliCommands[cleanedInput[0]]
+		if !ok{
+			fmt.Println("Unknown command")
+		} else{	
+			value.callback()
+		}
 	}
-	
 }
+
+func getCommands()map[string]cliCommand{
+	return map[string] cliCommand{
+		"exit":{
+			name: "exit",
+			description: "Exit the Pokedex",
+			callback: commandExit,
+		},
+		"help":{
+			name: "help",
+			description: "Displays a help message",
+			callback: commandHelp,
+		},
+	}
+}
+
 
 func cleanInput(text string) []string{
 	lowerdString := strings.ToLower(text)
@@ -25,8 +56,8 @@ func cleanInput(text string) []string{
 	t := strings.TrimSpace(lowerdString)
 
 	finalSlice:= strings.Split(t," ")
-
-	
-
 	return finalSlice
 }
+
+
+
